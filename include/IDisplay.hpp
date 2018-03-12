8c43aa676f70831b3e3e784486ad1a85bb1c22bd
Etienne <etienne.filliat@epitech.eu>
2018-03-12 15:26:12 +0100

    #3 - Add namespace for Display Interface

--- include/IDisplay.hpp
+++ include/IDisplay.hpp
@@ -10,25 +10,29 @@
 
 	#include <queue>
 
-class IDisplay {
-	public:
-		enum Keys {
-			MOVE_UP,
-			MOVE_DOWN,
-			MOVE_LEFT,
-			MOVE_RIGHT,
-			NEXT_LIB,
-			PREV_LIB,
-			NEXT_GAME,
-			PREV_GAME,
-			QUIT,
-		};
-		using KeysList = std::queue<Keys>;
+namespace arc {
+	class IDisplay;
+}
 
-		virtual ~IDisplay() = default;
-		virtual KeysList getKeys() = 0;
-		virtual void refresh() = 0;
-		virtual void clear() = 0;
+class IDisplay {
+public:
+	enum Keys {
+		MOVE_UP,
+		MOVE_DOWN,
+		MOVE_LEFT,
+		MOVE_RIGHT,
+		NEXT_LIB,
+		PREV_LIB,
+		NEXT_GAME,
+		PREV_GAME,
+		QUIT,
+	};
+	using KeysList = std::queue<Keys>;
+
+	virtual ~IDisplay() = default;
+	virtual KeysList getKeys() = 0;
+	virtual void refresh() = 0;
+	virtual void clear() = 0;
 };
 
 #endif /* !IDISPLAY_HPP_ */
