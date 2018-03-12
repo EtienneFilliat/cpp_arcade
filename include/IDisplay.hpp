1a4fffa34b8a2accaf85353188cefeef0c83b515
Etienne <etienne.filliat@epitech.eu>
2018-03-12 14:25:15 +0100

    #3 -  Simple Display Interface

--- include/IDisplay.hpp
+++ include/IDisplay.hpp
@@ -8,12 +8,27 @@
 #ifndef IDISPLAY_HPP_
 	#define IDISPLAY_HPP_
 
+	#include <queue>
+
 class IDisplay {
 	public:
-		IDisplay();
-		~IDisplay();
-	protected:
-	private:
+		enum Keys {
+			MOVE_UP,
+			MOVE_DOWN,
+			MOVE_LEFT,
+			MOVE_RIGHT,
+			NEXT_LIB,
+			PREV_LIB,
+			NEXT_GAME,
+			PREV_GAME,
+			QUIT,
+		};
+		using KeysList = std::queue<Keys>;
+
+		virtual ~IDisplay() = default;
+		virtual KeysList getKeys() = 0;
+		virtual void refresh() = 0;
+		virtual void clear() = 0;
 };
 
 #endif /* !IDISPLAY_HPP_ */
